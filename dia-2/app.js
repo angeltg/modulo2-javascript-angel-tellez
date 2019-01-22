33997dddff4b4b3b1bb2e410d1d4df930b3bdadb
angeltg <angeltellezgonzalez@gmail.com>
2019-01-22 21:44:35 +0100

    Fin de la clase

--- dia-2/app.js
+++ dia-2/app.js
@@ -3,3 +3,153 @@
  * practicar Javascript!
  */
 console.log("Probando, probando, 1, 2, 3!");
+// alert("asdsd" / 2);
+
+// let nombre = "Pepe";
+
+// alert(`hello, ${nombre}`);
+
+// let resultado = 2 < 1;
+
+// alert(resultado);
+
+// let user = null; le asignamos valor nulo
+
+// alert("usuario nulo " + user);
+
+let x; // si no la llenamos o no la declaramos sale undefined
+
+x = undefined; // esta palabra reservada es un valor, se puede asignar pero no se suele hacer
+
+// se usa para chequear campos. Si el user tiene el campo "cargo" es de la empresa, si es null o undefined es un suario externoß
+
+// alert("indefinido " + x);
+
+// alert(typeof null); // tupeof te da el tipo de varialblo o de dato que es.
+
+// Math //para operaciones matemáticas.
+
+// symbol  Tipo de variable para operadores únicos
+
+// let name = "Ilya";
+
+// alert(`Hello ${1}`); // Hello 1
+// alert(`Hello ${"name"}`); // Hello name
+// alert(`Hello ${name}`); // Hello Ilya
+
+// let value = true;
+
+// alert(typeof value);
+// value = String(value);
+
+// alert(typeof value);
+
+// let edad = "1234";
+
+// alert(typeof edad);
+
+// edad = Number(edad);
+
+// alert(typeof edad);
+
+// alert(Number("1234"));
+// alert(Number("123f"));
+// alert("1" + 2);
+// alert(1 + "2");
+
+// alert(Boolean("Hola"));
+// alert(Boolean(0));
+// alert(Boolean(""));
+
+// alert("" + 1 + 0); // 10
+// alert(1 + 0 + ""); // 10
+
+// alert("" - 1 + 0); // -1
+// alert(true + false); //  1
+// alert(6 / "3"); //  2
+// alert("2" * "3"); // 6
+// alert(4 + 5 + "px"); // 9px
+// alert("$" + 4 + 5); // $45
+// alert("4" - 2); // 2
+// alert("4px" - 2); //  NaN
+// alert(7 / 0); // Infinity
+// alert(" -9 " + 5); //  -9 5
+// alert(" -9 " - 5); //   -14
+// alert(null + 1); //  1
+// alert(undefined + 1); // NaN
+
+// let peras = "4";
+// let manzanas = "5";
+
+// let fruta;
+
+// fruta = +peras + +manzanas;
+// alert(typeof fruta + fruta);
+
+// alert(2 ** 3); //Se multiplica por si mismo el número de veces que indica el segundo valor. Es es lo mismo que 2*2*2 = 8;
+
+// let a = 1;
+
+// let b = 1;
+
+// let c = ++a; // 2
+// let d = b++; // 1
+
+// // a 2, b 2, c 2, d 1
+// alert(a + " " + b + " " + c + " " + d);
+
+// let a =2;
+
+// let x=1 + (a *=2);
+
+// a =4, x =5
+// alert("Ángel" > "Bngel");
+// alert(NaN == " ");
+
+// alert(5 > 4); //true
+// alert("apple" > "pineapple"); // false
+// alert("2" > "12"); // true
+// alert(undefined == null); // true
+// alert(undefined === null); // false
+// alert(null == "\n0\n"); // false
+// alert(null === +"\n0\n"); // false
+// alert(10 == "10"); //true
+// alert(10 === "10"); // false
+
+// let userName = prompt("¿Cual es tu nombre?", "Tu nombre");
+
+// alert(`Tu nombre es ${userName}`);
+// confirm("¿Quieres que te envíe actualizaciones?");
+
+// let anhos;
+// let edad = (edad > 18) ? true : false;
+// esto se pude simplificar asi:
+// let edad =(edad > 18);
+
+// if ("0") {
+//   alert("se muestra");
+//}
+
+// prompt
+// let nombre;
+// if (nomber == "ECMAScript") {
+//   alert("Right");
+// } else {
+//   alert("Did know");
+// }
+
+// let valor = prompt  ("un valor");
+
+// if  (valor >0) {
+//     alert(1)
+// }
+//
+
+// let message;
+
+// message = (login == 'Employee') ? 'Hello' : (login == 'Director')? 'Greetings': (login =='') ?'No login' : '';
+
+if (persona == "Manuela") {
+  ("Hola vecina");
+} else {
+}
