82ba778e1c241488b595228397179bc7179251e5
angeltg <angeltellezgonzalez@gmail.com>
2019-01-25 01:14:46 +0100

    Añadido script de conversión de enteros a números romanos y el cáculo de la letra del DNI

--- dia-4/app.js
+++ dia-4/app.js
@@ -70,30 +70,43 @@
 //   }
 // } while (!validado);
 
-// Hacer un numero entero en un numero romano.
+// Hacer la conversión de un numero entero en un numero romano.
+let parar = false;
 
-// let numero=10;
-// 1=I;
-// 2=II;
-// 3=III;
-// 4=IV;
-// 5=V;
-// 6=VI;
-// 7=VII;
-// 8=VIII;
-// 9=IX;
-// 10=X;
-// 11=XI;
+let arraydigitos = [
+  ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+  ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+  ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+];
 
-// let cadena;
-// let arraydigitos;
+function showCancel(parametro) {
+  return parametro == "" || parametro == null;
+}
+function getRomanNumber(numero) {
+  let cadenaromana = "";
+  let numeroseparado;
+  let posicion;
+  numeroseparado = numero.split("");
+  for (let i = 0; i < numeroseparado.length; i++) {
+    posicion = i + 1;
+    cadenaromana +=
+      arraydigitos[numeroseparado.length - posicion][numeroseparado[i]];
+  }
+  return cadenaromana;
+}
 
-// function conversion(numero){
-
-//     cadena= numero.toString();
-//     arraydigitos = cadena.split();
-
-// }
+do {
+  let numero = prompt("introduzca un número", "");
+  if (showCancel(numero)) {
+    alert("Has cancelado el proceso");
+    parar = true;
+  } else if (+numero < 1000)
+    alert("El número " + numero + " en romano es: " + getRomanNumber(numero));
+  else
+    alert(
+      "El número de ser menor de 1.000. Solo llevo 4 días con javascript..."
+    );
+} while (!parar);
 
 // console.log
 
