6c530c69bc103c307f6955a86bc9408231215e28
angeltg <angeltellezgonzalez@gmail.com>
2019-01-25 14:02:04 +0100

    Creado el .js avanzado para subir ejercicios fuera de temario

--- dia-4/app.js
+++ dia-4/app.js
@@ -30,123 +30,44 @@
 
 // User & Pasword
 
-// let userName;
-// let userPassword;
-// let parametro;
-// let validado = false;
+let userName;
+let userPassword;
+let parametro;
+let validado = false;
 
-// do {
-//   userName = prompt("Ingrese su nombre de usuario", "");
+do {
+  userName = prompt("Ingrese su nombre de usuario", "");
 
-//   function getPassword() {
-//     userPassword = prompt("Ingrese su password", "");
-//     return userPassword;
-//   }
+  function getPassword() {
+    userPassword = prompt("Ingrese su password", "");
+    return userPassword;
+  }
 
-//   function evaluePassword() {
-//     return userPassword == "TheMaster";
-//   }
+  function evaluePassword() {
+    return userPassword == "TheMaster";
+  }
 
-//   function showCancel(parametro) {
-//     return parametro == "" || parametro == null;
-//   }
+  function showCancel(parametro) {
+    return parametro == "" || parametro == null;
+  }
 
-//   if (userName == "Admin") {
-//     let result = evaluePassword(getPassword());
-//     if (result) {
-//       alert("Welcome!");
-//       validado = true;
-//     } else {
-//       if (showCancel(userPassword)) {
-//         alert("Has cancelado el proceso");
-//         validado = true;
-//       } else {
-//         alert("Sus datos no son correctos. Vuelva a intentarlo.");
-//       }
-//     }
-//   } else {
-//     if (showCancel(userName)) {
-//       alert("Has cancelado el proceso");
-//       validado = true;
-//     } else alert("No te conozco, vuelve a intentarlo.");
-//   }
-// } while (!validado);
-
-// // Hacer la conversión de un numero entero en un numero romano.
-// let parar = false;
-
-// let arraydigitos = [
-//   ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
-//   ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
-//   ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
-// ];
-
-// function showCancel(parametro) {
-//   return parametro == "" || parametro == null;
-// }
-// function getRomanNumber(numero) {
-//   let cadenaromana = "";
-//   let numeroseparado;
-//   let posicion;
-//   numeroseparado = numero.split("");
-//   for (let i = 0; i < numeroseparado.length; i++) {
-//     posicion = i + 1;
-//     cadenaromana +=
-//       arraydigitos[numeroseparado.length - posicion][numeroseparado[i]];
-//   }
-//   return cadenaromana;
-// }
-
-// do {
-//   let numero = prompt("introduzca un número", "");
-//   if (showCancel(numero)) {
-//     alert("Has cancelado el proceso");
-//     parar = true;
-//   } else if (+numero < 1000 && +numero > 0)
-//     alert("El número " + numero + " en romano es: " + getRomanNumber(numero));
-//   else
-//     alert(
-//       "El número de ser menor de 1.000 y mayor de 0. Sólo llevo 4 días con javascript..."
-//     );
-// } while (!parar);
-
-// console.log
-
-// SCript para saber la letra del DNI
-
-let dni = 32818535;
-
-let resto = dni % 23;
-
-function getletter(resto) {
-  let arrayLetters = [
-    "t",
-    "r",
-    "w",
-    "a",
-    "g",
-    "m",
-    "y",
-    "f",
-    "p",
-    "d",
-    "x",
-    "b",
-    "n",
-    "j",
-    "z",
-    "s",
-    "q",
-    "v",
-    "h",
-    "l",
-    "c",
-    "k",
-    "e"
-  ];
-  return arrayLetters[resto];
-}
-
-console.log("Valor " + getletter(resto));
-
-// cintonic
+  if (userName == "Admin") {
+    let result = evaluePassword(getPassword());
+    if (result) {
+      alert("Welcome!");
+      validado = true;
+    } else {
+      if (showCancel(userPassword)) {
+        alert("Has cancelado el proceso");
+        validado = true;
+      } else {
+        alert("Sus datos no son correctos. Vuelva a intentarlo.");
+      }
+    }
+  } else {
+    if (showCancel(userName)) {
+      alert("Has cancelado el proceso");
+      validado = true;
+    } else alert("No te conozco, vuelve a intentarlo.");
+  }
+} while (!validado);
