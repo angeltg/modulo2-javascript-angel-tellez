572eeb2e1190aa0e4ccc36fe5f6152c01da05af2
angeltg <angeltellezgonzalez@gmail.com>
2019-01-27 23:38:19 +0100

    Ejercicio con la class Person

--- dia-6/appExtra.js
+++ dia-6/appExtra.js
@@ -75,17 +75,108 @@
 // let naranja = new Fruta("Naranja", 3);
 // console.log(naranja.infoPrecio());
 
-class Fruta {
-  constructor(nombre, precio) {
-    this.nombre = nombre;
-    this.precio = precio;
+// class Fruta {
+//   constructor(nombre, precio) {
+//     this.nombre = nombre;
+//     this.precio = precio;
+//   }
+//   infoPrecio() {
+//     return "El precio de la " + this.nombre + " es " + this.precio;
+//   }
+// }
+
+// let manzana = new Fruta("Manazana", 2);
+// console.log(manzana.infoPrecio());
+// let pera = new Fruta("Pera", 3);
+// console.log(pera.infoPrecio());
+
+/////////////////////////////////////////////////////////
+
+// Crear una clase llamada Person que acepte un monbre y una edad
+
+// La clase Persona deberá tener un método que devuelva un string con "nombre, tiene edad años". Por ejemplo "Pepe tiene 25 años".
+
+// Otro método que sume 1 a la edad de la persona.
+
+// Para cargar los datos de la clase pedimos por prompt nombres y edades hasta que pulse esc o lo llene vacío.
+// Mostramos resultado por la consola.
+// Para sumar 1 año a una persona pedimos por prompt que nos diga un monbre
+// Si ese nombre existe le decimos la edad que tiene y preguntamos si quiere sumarle un año. Si quiere se lo sumamos y si no le preguntamos por otro nombre.
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
   }
-  infoPrecio() {
-    return "El precio de la " + this.nombre + " es " + this.precio;
+  get_message() {
+    return this.name + " tiene " + this.age + " años ";
+  }
+  set_anho() {
+    this.age++;
   }
 }
 
-let manzana = new Fruta("Manazana", 2);
-console.log(manzana.infoPrecio());
-let pera = new Fruta("Pera", 3);
-console.log(pera.infoPrecio());
+let numPersons = 0;
+let personVar;
+function createPerson(indentificator) {
+  personVar = "person" + indentificator;
+  let personName;
+  let personAge;
+  personName = prompt("Introduzca el nombre de la persona");
+  personAge = prompt("Introduzca la edad de la persona");
+
+  if (personName != "" && personAge > 0) {
+    eval(personVar + " = new Person (personName,personAge)");
+    return true;
+  } else {
+    return false;
+  }
+}
+let cargarDatos = true;
+for (let i = 0; cargarDatos; i++) {
+  cargarDatos = createPerson(i);
+  if (cargarDatos) {
+    console.log(eval(personVar).name + " " + eval(personVar).age);
+    numPersons++;
+  }
+}
+
+let operator;
+let correctparameter = false;
+function searchPerson(operator) {
+  for (let i = 0; i <= numPersons; i++) {
+    let personVar = "person" + i;
+    if (eval(personVar).name == operator) {
+      return eval(personVar);
+    }
+  }
+  return false;
+}
+
+if (numPersons > 0) {
+  do {
+    operator = prompt(
+      "Introduzca el nombre de la persona de la que quiere saber su edad"
+    );
+
+    if (operator) {
+      let searchPersonAge = searchPerson(operator);
+      if (searchPersonAge) {
+        console.log(
+          "La edad de " + operator + " es de " + searchPersonAge.age + " años"
+        );
+        if (confirm("¿Desea añadirle un año a " + operator + "?")) {
+          searchPersonAge.set_anho();
+          console.log(
+            "Ahora " + operator + " tiene " + searchPersonAge.age + " años"
+          );
+        }
+      } else {
+        console.log("No existe ninguna persona con ese nombre.");
+      }
+    } else {
+      console.log("Has salido del programa.");
+      correctparameter = true;
+    }
+  } while (!correctparameter);
+}
