81a8ac950b760a0e55a2662309179c3eff039c46
angeltg <angeltellezgonzalez@gmail.com>
2019-01-27 11:16:00 +0100

    Todos los ejercicios de clase más los de appExtra.js

--- dia-6/app.js
+++ dia-6/app.js
@@ -80,6 +80,20 @@
 // calculator.sum();
 // calculator.mul();
 
+// Create new Accumulador
+
+// function Accumulator(startingValue) {
+//   (this.startingValue = startingValue),
+//     (this.read = function() {
+//       this.startingValue += +prompt("Valor a aumentar: ", 0);
+//     });
+// }
+
+// let accumulator = new Accumulator(1);
+// accumulator.read();
+// accumulator.read();
+// console.log(accumulator.startingValue);
+
 // Hay que retornar el ogj para poder encadenar los métodos
 // let ladder = {
 //   step: 0,
@@ -104,20 +118,103 @@
 
 //   funciton de saludar y un parametro a quien quieres saludor.
 
-// Para poder saludar el nombre tiene que tener una a.
+// Para poder saludar el nombre tiene que tener una p.
 
-function User(nameUser, funcion) {
-  (this.name = nameUser), (this.sayHi = funcion);
-}
+// function User(nameUser, funcion) {
+//   (this.name = nameUser), (this.sayHi = funcion);
+// }
 
-let user = new User("Pepe", function(nombre) {
-  return "Hola, soy " + nombre;
-});
-// console.log(user);s
-// user.sayHi();
+// let user = new User("Pepe", function(nombre) {
+//   return "Hola, soy " + nombre;
+// });
+// // console.log(user);
+// // user.sayHi();
 
-if (user.name.indexOf("p") >= 0) {
-  console.log(user.sayHi(user.name));
-} else {
-  console.log("Tu nombre no tiene a, por eso no saludas");
-}
+// if (user.name.indexOf("p") >= 0) {
+//   console.log(user.sayHi(user.name));
+// } else {
+//   console.log("Tu nombre no tiene a, por eso no saludas");
+// }
+
+// function Clock({ template }) {
+//   this._template = template;
+// }
+
+// Clock.prototype._render = function() {
+//   let date = new Date();
+
+//   let hours = date.getHours();
+//   if (hours < 10) hours = "0" + hours;
+
+//   let mins = date.getMinutes();
+//   if (mins < 10) mins = "0" + mins;
+
+//   let secs = date.getSeconds();
+//   if (secs < 10) secs = "0" + secs;
+
+//   let output = this._template
+//     .replace("h", hours)
+//     .replace("m", mins)
+//     .replace("s", secs);
+
+//   console.log(output);
+// };
+
+// Clock.prototype.stop = function() {
+//   clearInterval(this._timer);
+// };
+
+// Clock.prototype.start = function() {
+//   this._render();
+//   this._timer = setInterval(() => this._render(), 1000);
+// };
+
+// class ClockClass {
+//   constructor({ template }) {
+//     this._template = template;
+//   }
+
+//   _render() {
+//     let date = new Date();
+//     let hours = date.getHours();
+//     if (hours < 10) hours = "0" + hours;
+
+//     let mins = date.getMinutes();
+//     if (mins < 10) mins = "0" + mins;
+
+//     let secs = date.getSeconds();
+//     if (secs < 10) secs = "0" + secs;
+//     // console.log(typeof this._template);
+
+//     let output = this._template
+//       .replace("h", hours)
+//       .replace("m", mins)
+//       .replace("s", secs);
+
+//     console.log(output);
+//   }
+//   stop() {
+//     clearInterval(this._timer);
+//   }
+//   start(precision) {
+//     this._render();
+//     this._timer = setInterval(() => this._render(), precision);
+//   }
+// }
+
+// class ColckClassExtend extends ClockClass {
+//   constructor(obj) {
+//     super(obj);
+//     this.precision = obj.precision;
+//   }
+//   start() {
+//     super._render();
+//     this._timer = setInterval(() => this._render(), this.precision);
+//   }
+// }
+
+// let lowResolutionClock = new ColckClassExtend({
+//   template: "h:m:s",
+//   precision: 5000
+// });
+// lowResolutionClock.start();
--- dia-6/appExtra.js
+++ dia-6/appExtra.js
@@ -1,20 +1,21 @@
 // Crear una expresión regular valide una fecha en formato "XX/XX/XXXX", donde "X" es un dígito. Probarlo con la expresión: "Nací el 05/04/1982 en Donostia.".
 
-function validateDate(str) {
-  let arraystr = str.split("/");
-  let dia = arraystr[0].substr(-2, 2);
-  let mes = arraystr[1];
-  let anho = arraystr[2].substr(0, 4);
+// function validateDate(str) {
+//   let arraystr = str.split("/");
+//   let dia = arraystr[0].substr(-2, 2);
+//   let mes = arraystr[1];
+//   let anho = arraystr[2].substr(0, 4);
 
-  let date = dia + "/" + mes + "/" + anho;
-  return date;
-}
+//   let date = dia + "/" + mes + "/" + anho;
+//   return date;
+// }
 
-console.log(validateDate("Nací el 05/04/1982 en Donostia."));
+// console.log(validateDate("Nací el 05/04/1982 en Donostia."));
 
 // function lanzardatos() {
-//   let valor1 = Math.random(1, 6).Math.round();
-//   return valor1;
+//   let valor1 = Math.random();
+//   valor2 = Math.round(valor1);
+//   return valor2;
 // }
 
 // console.log(lanzardatos());
@@ -23,23 +24,68 @@ console.log(validateDate("Nací el 05/04/1982 en Donostia."));
 
 // Dados dos objectos, declara una función que me diga cuál de los dos objectos es que el que está vacío.
 
-function isEmptyObj(obj) {
-  for (let key in obj) {
-    return obj.hasOwnProperty(key);
+// function isEmptyObj(obj) {
+//   for (let key in obj) {
+//     return obj.hasOwnProperty(key);
+//   }
+// }
+
+// let nadaAqui = {};
+// let algoAqui = {
+//   unparameter: "Uno"
+// };
+
+// if (!isEmptyObj(nadaAqui)) {
+//   console.log("nadaAqui es el objeto que está vacío");
+// } else {
+//   if (!isEmptyObj(algoAqui)) {
+//     console.log("algoAqui es el objeto que está vacío");
+//   } else {
+//     console.log("Los dos objetos están llenos");
+//   }
+// }
+///////////////////////////////////////////////
+// Así lo hizo Aitor
+// function estaVacio(map) {
+//     for(var key in map) {
+//       if (map.hasOwnProperty(key)) {
+//          return false;
+//       }
+//     }
+//     return true;
+//  }
+
+//  console.log(estaVacio(nadaAqui));
+
+///////////////////////////////////////////////////////////////////////
+
+// Pasar este código a ES6 classes.
+// let Fruta = function(nombre, precio) {
+//   this.nombre = nombre;
+//   this.precio = precio;
+// };
+
+// Fruta.prototype.infoPrecio = function() {
+//   return `El precio de ${this.nombre} es ${this.precio}€`;
+// };
+
+// let manzana = new Fruta("Manzana", 2);
+// console.log(manzana.infoPrecio());
+
+// let naranja = new Fruta("Naranja", 3);
+// console.log(naranja.infoPrecio());
+
+class Fruta {
+  constructor(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+  infoPrecio() {
+    return "El precio de la " + this.nombre + " es " + this.precio;
   }
 }
 
-let nadaAqui = {};
-let algoAqui = {
-  unparameter: "Uno"
-};
-
-if (!isEmptyObj(nadaAqui)) {
-  console.log("nadaAqui es el objeto que está vacío");
-} else {
-  if (!isEmptyObj(algoAqui)) {
-    console.log("algoAqui es el objeto que está vacío");
-  } else {
-    console.log("Los dos objetos están llenos");
-  }
-}
+let manzana = new Fruta("Manazana", 2);
+console.log(manzana.infoPrecio());
+let pera = new Fruta("Pera", 3);
+console.log(pera.infoPrecio());
