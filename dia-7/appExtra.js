50e255ce3dd4e4c0be85e2d9b56053baf8b4878f
angeltg <angeltellezgonzalez@gmail.com>
2019-01-29 16:53:58 +0100

    Funcion xor

--- dia-7/appExtra.js
+++ dia-7/appExtra.js
@@ -20,27 +20,42 @@
 
 // Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
 
-class Student {
-  constructor(name, sclass, rollno) {
-    this.name = name;
-    this.sclass = sclass;
-    this.rollno = rollno;
-  }
-  listProperties() {
-    for (const key in this) {
-      console.log(key + " " + this[key]);
-    }
-  }
-}
+// class Student {
+//   constructor(name, sclass, rollno) {
+//     this.name = name;
+//     this.sclass = sclass;
+//     this.rollno = rollno;
+//   }
+//   listProperties() {
+//     for (const key in this) {
+//       console.log(key + " " + this[key]);
+//     }
+//   }
+// }
 
-let student1 = new Student("Lourdes", "3A", "Buen estudiante");
-student1.listProperties();
+// let student1 = new Student("Lourdes", "3A", "Buen estudiante");
+// student1.listProperties();
 
-console.log("Añadimos una propiedad con bark");
-Student.bark = "secondname";
-student1.secondname = "Rodriguez";
-student1.listProperties();
-console.log("Borramos una propiedad de la clase con delete");
-let deleteproperty = "rollno";
-delete Student.rollno;
-student1.listProperties();
+// console.log("Añadimos una propiedad con bark");
+// Student.bark = "secondname";
+// student1.secondname = "Rodriguez";
+// student1.listProperties();
+// console.log("Borramos una propiedad de la clase con delete");
+// let deleteproperty = "rollno";
+// delete Student.rollno;
+// student1.listProperties();
+
+// Función xor que no funciona si se le pasa un objeto boolean.
+
+//Cualquier ogjeto que no sea undefined o null es true, aunque el objeto sea boolean. Así que pasamos el valor del objeto a una variable y ya tenemos su valor para poder compararlo
+
+Boolean.prototype.xor = function(value) {
+  let valor;
+  if (this == false) {
+    valor = false;
+  } else valor = true;
+  console.log(valor);
+  return !!valor !== !!value;
+};
+
+console.log(false.xor(false)); // => true
