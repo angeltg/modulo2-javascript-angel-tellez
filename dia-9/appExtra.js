483dd45cae5313c4e0b89a8438eca05b802720c2
angeltg <angeltellezgonzalez@gmail.com>
2019-01-31 17:36:12 +0100

    Función recursiva

--- dia-9/appExtra.js
+++ dia-9/appExtra.js
@@ -7,14 +7,6 @@
  *
  */
 
-/**
- * Crear una funcion que al pasarle un objeto te de toda la informacion sobre el.
- *
- * Cuantas propiedades tiene, que valen sus propiedades, si el valor de una propiedad es un
- * string que me diga la longitud del string, si tiene un objeto dentro que entre dentro de el objeto
- * y lo analice tambien y asi sucesivamente. (Pista: RECURSIVIDAD)
- */
-
 class Person {
   constructor(name, apellido, casadocon) {
     this.name = name;
@@ -24,27 +16,42 @@ class Person {
   static marriedWith(person1, person2) {
     return person1.casadocon.toUpperCase() == person2.name.toUpperCase();
   }
-  totalinfo() {
-    let i = 0;
-    // muestra todas el número de propiedades, functions y herencias
-    console.log(Object.getPrototypeOf(this));
-    // muetra las propiedades y el número
-    console.log(Object.getOwnPropertyNames(this));
-
-    //mostramos el valor de cada propiedad
-    for (const key in this) {
-      i++;
-      console.log(key + " = " + this[key]);
-      if (typeof this[key] == "string") {
-        console.log("Longitud del string " + this[key].length);
-      }
-    }
-    // console.log("Número de propiedades:" + i);
-  }
 }
 
 let person1 = new Person("Pepe", "López", "JUana");
-let person2 = new Person("Juana", "Rodriguez", "Pepe");
+// let person2 = new Person("Juana", "Rodriguez", "Pepe");
 
-console.log(Person.marriedWith(person1, person2));
-person1.totalinfo();
+// console.log(Person.marriedWith(person1, person2));
+
+/**
+ * Crear una funcion que al pasarle un objeto te de toda la informacion sobre el.
+ *
+ * Cuantas propiedades tiene, que valen sus propiedades, si el valor de una propiedad es un
+ * string que me diga la longitud del string, si tiene un objeto dentro que entre dentro de el objeto
+ * y lo analice tambien y asi sucesivamente. (Pista: RECURSIVIDAD)
+ */
+
+function totalinfo(obj) {
+  let i = 0;
+  // muestra todas el número de propiedades, functions y herencias
+  // console.log(Object.getPrototypeOf(obj));
+  // muetra las propiedades y el número
+  // console.log(Object.getOwnPropertyNames(obj));
+
+  // si es string mostramos su longitud
+  if (typeof obj == "string") {
+    console.log("Longitud del string " + obj.length);
+  } else {
+    //mostramos el valor de cada propiedad
+
+    for (const key in obj) {
+      i++;
+      console.log(key + " = " + obj[key]);
+      totalinfo(key);
+    }
+    console.log("Número de propiedades:" + i);
+  }
+}
+
+let str = "sdasda asdasdadsa";
+totalinfo(person1);
